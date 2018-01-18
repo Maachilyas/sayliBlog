ae16072db3b6dc807017184a9ee4a8feb6b7d7f4
Maachilyas <maachilyaas@gmail.com>
2018-01-18 14:23:17 +0000

    setup tdd env, auth scaffolding

---
+++ tests/helpers.php
@@ -0,0 +1,13 @@
+<?php
+
+/* helper functions for testing */
+
+function create($class, $attributes = [], $count = null)
+{
+    return factory($class, $count)->create($attributes);
+}
+
+function make($class, $attributes = [], $count = null)
+{
+    return factory($class, $count)->make($attributes);
+}
